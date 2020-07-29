6353156b7e01e8133ca1e170b7e010fcc45c56e0
HiepCongChua <hieplevuc@gmail.com>
2020-07-29 14:46:02 +0700

    complete init project

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express")
 const app = express()
 const port = 3000
-
+const socketIo = require("socket.io")
+const helmet = require("helmet")
 app.get("/", (req, res) => res.send("Hello World!"))
-
-app.listen(port, () =>
+app.use(express.static(__dirname + "/public"))
+const expressServer = app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 )
+const io = socketIo(expressServer) // Atach socket server to http server
+app.use(helmet())
+io.sockets.on("connection", (socket) => {
+  socket.on("disconnect", (data) => {})
+  socket.on("dataFromClient", () => {
+    socket.emit("event", {}, () => {})
+  })
+  socket.on("", () => {})
+})
+app.get("/stats", () => {})
+module.exports = {
+  app,
+  io,
+}
